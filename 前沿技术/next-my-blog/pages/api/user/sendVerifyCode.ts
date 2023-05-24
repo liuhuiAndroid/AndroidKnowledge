6bd5c88c2e2b8d0b9e4f1faab9d24af8d641fe0c
lh <im.lh@hotmail.com>
2023-05-24 17:23:09 +0800

    Next.js

---
+++ 前沿技术/next-my-blog/pages/api/user/sendVerifyCode.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { format } from 'date-fns';
+import md5 from 'md5';
+import { encode } from 'js-base64';
+import request from 'service/fetch';
+import { ISession } from 'pages/api/index';
+import { ironOptions } from 'config/index';
+
+export default withIronSessionApiRoute(sendVerifyCode, ironOptions);
+
+async function sendVerifyCode(req: NextApiRequest, res: NextApiResponse) {
+  const session: ISession = req.session;
+  const { to = '', templateId = '1' } = req.body;
+  const AppId = '8aaf07087d7fb5f6017d950ce83f04e1';
+  const AccountId = '8aaf07087d7fb5f6017d950ce72c04da';
+  const AuthToken = '91725ff244364cda9f1e1ea7d471e124';
+  const NowDate = format(new Date(), 'yyyyMMddHHmmss');
+  const SigParameter = md5(`${AccountId}${AuthToken}${NowDate}`);
+  const Authorization = encode(`${AccountId}:${NowDate}`);
+  const verifyCode = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+  const expireMinute = '5';
+  const url = `https://app.cloopen.com:8883/2013-12-26/Accounts/${AccountId}/SMS/TemplateSMS?sig=${SigParameter}`;
+
+  const response = await request.post(
+    url,
+    {
+      to,
+      templateId,
+      appId: AppId,
+      datas: [verifyCode, expireMinute],
+    },
+    {
+      headers: {
+        Authorization,
+      },
+    }
+  );
+
+  console.log(verifyCode)
+  console.log(response);
+  const { statusCode, templateSMS, statusMsg } = response as any;
+
+  if (statusCode === '000000') {
+    session.verifyCode = verifyCode;
+    await session.save();
+    res.status(200).json({
+      code: 0,
+      msg: statusMsg,
+      data: {
+        templateSMS
+      }
+    });
+  } else {
+    res.status(200).json({
+      code: statusCode,
+      msg: statusMsg
+    });
+  }
+}
