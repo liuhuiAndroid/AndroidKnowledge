08980992b72b8e23634669494b95a153c45b3954
lh <im.lh@hotmail.com>
2023-08-06 11:10:54 +0800

    xml2json test

--- Android Cross-platform/flutter_network/lib/http_ican.dart
+++ Android Cross-platform/flutter_network/lib/http_ican.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_network/data_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:xml2json/xml2json.dart';
 
 /// stf
 /// flutter pub add http
@@ -18,6 +19,9 @@ class _HttpICanState extends State<HttpICan> {
 
   get _doGetBtn => ElevatedButton(
       onPressed: _doGetRequest, child: const Text('send get request'));
+
+  get _doGetXmlBtn => ElevatedButton(
+      onPressed: _doGetXmlRequest, child: const Text('send get request'));
 
   get _doPostBtn => ElevatedButton(
       onPressed: _doPostRequest, child: const Text('send post request'));
@@ -35,6 +39,7 @@ class _HttpICanState extends State<HttpICan> {
       body: Column(
         children: [
           _doGetBtn,
+          _doGetXmlBtn,
           _doPostBtn,
           _doPostJson,
           Text('result: $resultShow')
@@ -45,12 +50,49 @@ class _HttpICanState extends State<HttpICan> {
 
   /// send get request
   Future<void> _doGetRequest() async {
-    var uri =
-        Uri.parse('https://api.devio.org/uapi/test/test?requestPrams=ChatGPT2');
+    var uri = Uri.parse(
+        'https://www.maigel.com/servlet/async.ashx?operationType=17&publisher=zhanhui210609');
+
     var response = await http.get(uri);
     if (response.statusCode == 200) {
       setState(() {
         resultShow = response.body;
+      });
+    } else {
+      setState(() {
+        resultShow = "请求失败：code=${response.statusCode}, body=${response.body}";
+      });
+    }
+  }
+
+  /// send get request
+  Future<void> _doGetXmlRequest() async {
+    var uri = Uri.parse('http://live.maigel.com/videos/103482.xml');
+
+    var response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final myTransformer = Xml2Json();
+      String result = response.body;
+      myTransformer.parse(result);
+      var json1 = myTransformer.toBadgerfish();
+      debugPrint('json1$json1');
+
+      myTransformer.parse(result);
+      var json2 = myTransformer.toGData();
+      Utf8Decoder utf8decoder = const Utf8Decoder();
+      json2 = utf8decoder.convert(json2.codeUnits);
+      debugPrint('json2$json2');
+
+      myTransformer.parse(result);
+      var json3 = myTransformer.toParker();
+      debugPrint('json3$json3');
+
+      myTransformer.parse(result);
+      var json4 = myTransformer.toParkerWithAttrs();
+      debugPrint('json4$json4');
+
+      setState(() {
+        resultShow = jsonEncode(json2);
       });
     } else {
       setState(() {
